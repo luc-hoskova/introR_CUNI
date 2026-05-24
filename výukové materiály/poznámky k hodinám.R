309018f92218f5397cbac369d560abda003f4049
luc-hoskova <lucie.hoskova@email.cz>
2026-05-24 19:52:14 +0200

    update

---
+++ výukové materiály/poznámky k hodinám.R
@@ -0,0 +1,41 @@
+#kniha jazyk r a tvorba grafů
+
+#1 hodina
+#operátory
+  #matematické
+  #!, &, %in%, :, ten znak pro nebo
+#ukládání do proměných
+#etiketa psaní kódu
+#ukládání souborů
+  #projekty
+  #RData, workspace, load()
+
+#2 hodina
+#x[3] <- 10
+  #nahrazení třetí pozice v x číslem 10
+#sort() a order()
+#názvy sloupců a řádků
+#přidání sloupců a řádků
+#dataframe vs tibble
+
+#3 hodina
+#faktory
+  #ukládá se to jako číslo tzn když se dá as.numeric() tak je to zobrazí tak jak je to srovnané (asi podle abecedy)
+
+#4 hodina
+#if-else, for, while
+
+#7 hodina
+#interaktivní grafy pomocí plotly
+#grafy z ggplo2 lze převést na interaktivní pomocí funkce plotly::ggplotly()
+
+#9 hodina
+#balíček *psych*
+
+library(psych)
+
+# naměřené hodnoty
+data <- c(3, 5, 6, 7, 8)
+
+# výpočet souboru popisných charakteristik s funkcí describe
+describe(data)
