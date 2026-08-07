a22c94ae46c2f8ed40675613bf89757c22e9b6aa
luc-hoskova <lucie.hoskova@email.cz>
2026-08-07 18:10:35 +0200

    update materiály

---
+++ materialy/02_struktury/02_poznamky.R
@@ -0,0 +1,17 @@
+#SAMOSTATNE CVICENI VEKTORY
+#1
+c(1,2,3,1,2,3,9,8,7,2)
+c(rep(1:3, 2), 9:7, 2)
+c(rep(1:3, 2), seq(9, 7), 2)
+#2
+#names(kolo) <- c("jmeno", "prijmeni)
+#fce: names(kolo) <- tolower(names(kolo))
+
+#as.typ dat()
+
+#INDEXACE
+#můžeme si polohu vybrat a pak ji upravit, tím pádem i přidat
+#pamatujete na přepisování?
+#Přidávání a odebírání hodnot podle pozice
+#y <- 1:5
+#y[6] <- 7; y
---
+++ materialy/03_transformace/03_poznamky.R
@@ -0,0 +1,4 @@
+#grep funkce
+#https://www.statology.org/r-grep-match-replace/
+#slouží k vyhledávání vzorů ve vektorech a jejich nahrazení
+#práce s vektorem jako s textem
