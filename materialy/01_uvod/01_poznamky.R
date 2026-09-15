8215be597f8cda8e230d8286d640d04ced7d006f
luc-hoskova <lucie.hoskova@email.cz>
2026-09-15 10:11:09 +0200

    oprava odkaz rstudio

---
+++ materialy/01_uvod/01_poznamky.R
@@ -0,0 +1,40 @@
+#1. LEKCE
+#PROCVICENI PRIRAZOVANI
+#1
+x <- 5
+y <- "auto"
+z <- TRUE;
+
+x
+y; z
+
+w <- 7; w
+
+#PROCVICENI OPERATORU
+#1
+(9*10)%%4
+
+#2
+6 ^ 2 - 3 * 5
+(6 ^ 2) - (3 * 5)
+
+#3
+vek <- 13
+povoleni <- FALSE
+vek >= 15 | povoleni == TRUE
+
+#SAMOSTATNE CVICENI OPERATORY
+#1
+53 <= 26 | 53 >= 65
+
+#2
+prumer <- 1.7
+dochazka <- 70
+reprezentace <- TRUE
+prumer < 1.8 & dochazka > 80 | prumer < 1.8 & reprezentace == TRUE
+
+#3
+znamka <- 5
+pocetBodu <- 17
+semestralka <- TRUE
+znamka < 5 & pocetBodu != 0 | semestralka == TRUE & pocetBodu >= 50
