e39df21b47de4d0cafc14ba5e100732b5db4bdd3
luc-hoskova <lucie.hoskova@email.cz>
2026-09-17 11:17:32 +0200

    ukoly

---
+++ domaciUkoly/01/DU-zdenka_drastichová/DU1/DU1.R
@@ -0,0 +1,13 @@
+rozpocet/pocetMesicu
+91580 %/% plat #odkud se vzalo cislo 91580?
+  #pokud chces pouzit vysledek vypoctu, tak ho lze pouzit rovnou
+    #a nebo si ho ulozit
+
+#1. moznost
+penizeNaMesic <- rozpocet/pocetMesicu
+penizeNaMesic %/% plat #3
+
+#2. moznost
+(rozpocet/pocetMesicu) %/% plat #3
+
+# Z rozpoctu lze platit po dobu 6 mesicu 3 zamestnance.
